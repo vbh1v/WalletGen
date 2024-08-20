a2a433bc030fcf26c0e425d4130127603fad5902
Vaibhav <vaibhavvsharma@hotmail.com>
2024-08-21 02:54:47 +0530

    changed footer...

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <section className="max-w-7xl mx-auto border-t px-4">
       <div className="flex justify-between py-8">
         <p className="text-primary tracking-tight">
-          Designed and Developed by{" "}
+          Developed by{" "}
           <Link href={"https://github.com/vbh1v"} className="font-bold">
             Vaibhav
           </Link>
